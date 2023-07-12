56621ba2f33c7193615a5d4dae3ae895faf47833
jonathannalencar <jonathannalencar@gmail.com>
2023-07-11 22:18:47 -0300

    GET list by Id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.dto.GameDTO;
 import com.devsuperior.dslist.entities.dto.GameMinDTO;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -30,4 +31,13 @@ public class GameService {
 		return dto;
 		//return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+		//return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
 }
